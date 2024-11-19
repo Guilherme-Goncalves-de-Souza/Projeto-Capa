addee15feab8182649977068b8743ab59a196c7e
Thiago Vasconcelos <thiagovt.dev@gmail.com>
2024-11-19 01:04:06 -0300

    Enhance database configuration by adding support for Unicamp connection and improving formatting

--- capa-award-api/capa-award-api-unicamp/strapi-app/config/database.js
+++ capa-award-api/capa-award-api-unicamp/strapi-app/config/database.js
@@ -1,25 +1,47 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        host: env('DATABASE_HOST'),
-        client: 'mysql',
-        port: parseInt(env('DATABASE_PORT')),
-        database: env('DATABASE_DB'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
+        host: env("DATABASE_HOST"),
+        client: "mysql",
+        port: parseInt(env("DATABASE_PORT")),
+        database: env("DATABASE_DB"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
       },
       options: {
         ssl: true,
-        insecureAuth : true,
+        insecureAuth: true,
         pool: {
           min: 2,
           max: 20,
           idleTimeoutMillis: 30000,
           createTimeoutMillis: 30000,
-          acquireTimeoutMillis: 30000
+          acquireTimeoutMillis: 30000,
+        },
+      },
+    },
+    unicamp: {
+      connector: "bookshelf",
+      settings: {
+        host: env("DATABASE_HOST_UNICAMP"),
+        client: "mysql",
+        port: parseInt(env("DATABASE_PORT_UNICAMP")),
+        database: env("DATABASE_DB_UNICAMP"),
+        username: env("DATABASE_USERNAME_UNICAMP"),
+        password: env("DATABASE_PASSWORD_UNICAMP"),
+      },
+      options: {
+        ssl: true,
+        insecureAuth: true,
+        pool: {
+          min: 2,
+          max: 20,
+          idleTimeoutMillis: 60000,
+          createTimeoutMillis: 60000,
+          acquireTimeoutMillis: 60000,
         },
       },
     },
