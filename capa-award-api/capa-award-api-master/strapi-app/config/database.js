7a4e822d8f16b02c416d35077bb8e8405af0135d
Thiago Vasconcelos <thiagovt.dev@gmail.com>
2024-11-22 14:39:28 -0300

    revert: database max time

--- capa-award-api/capa-award-api-master/strapi-app/config/database.js
+++ capa-award-api/capa-award-api-master/strapi-app/config/database.js
@@ -16,7 +16,7 @@ module.exports = ({ env }) => ({
         insecureAuth: true,
         pool: {
           min: 2,
-          max: 5,
+          max: 10,
           idleTimeoutMillis: 30000,
           createTimeoutMillis: 30000,
           acquireTimeoutMillis: 30000,
